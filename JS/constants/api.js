1e6ec64520a2a06733ff6ec99f37424745a22d3e
Thomas Eitrheim <teitrheim@gmail.com>
2023-09-14 07:39:41 +0200

    updating JS

---
+++ JS/constants/api.js
@@ -0,0 +1,5 @@
+const apiBase = "https://cors.noroff.dev/https://fightingdays.seeorno.no";
+const woocommerceBase = "/wp-json/wp/v2";
+const productBase = "/posts";
+
+export const fullProductUrl = apiBase + woocommerceBase + productBase;
---
+++ JS/index.js
@@ -0,0 +1,83 @@
+const fullProductUrl = "https://fightingdays.seeorno.no/wp-json/wp/v2/posts";
+
+async function getProducts() {
+  try {
+    const response = await fetch(fullProductUrl);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const products = await response.json();
+    return products;
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    return [];
+  }
+}
+
+function createProductHtml(product) {
+  const container = document.querySelector(".all-products");
+  const productContainer = document.createElement("div");
+  productContainer.classList.add("product");
+
+  const productLink = document.createElement("a");
+  productLink.classList.add("product-item");
+  productLink.href = `specificpage.html?id=${product.id}`;
+
+  const title = document.createElement("h3");
+  title.textContent = product.title.rendered;
+
+  productLink.appendChild(title);
+
+  if (product.featured_media) {
+    const image = document.createElement("img");
+    image.src = product.featured_media;
+    image.alt = product.title.rendered;
+    productLink.appendChild(image);
+  }
+
+  productContainer.appendChild(productLink);
+
+  container.appendChild(productContainer);
+}
+
+function createFeaturedProductHtml(product) {
+  const container = document.querySelector(".featured-products");
+  const productContainer = document.createElement("div");
+  productContainer.classList.add("product");
+
+  const productLink = document.createElement("a");
+  productLink.classList.add("product-item");
+  productLink.href = `specificpage.html?id=${product.id}`;
+
+  const title = document.createElement("h3");
+  title.textContent = product.title.rendered;
+
+  productLink.appendChild(title);
+
+  if (product.featured_media) {
+    const image = document.createElement("img");
+    image.src = product.featured_media;
+    image.alt = product.title.rendered;
+    productLink.appendChild(image);
+  }
+
+  productContainer.appendChild(productLink);
+
+  container.appendChild(productContainer);
+}
+
+async function shopPage() {
+  const products = await getProducts();
+
+  for (let i = 0; i < products.length; i++) {
+    const product = products[i];
+
+    if (product.featured) {
+      createFeaturedProductHtml(product);
+    } else {
+      createProductHtml(product);
+    }
+  }
+}
+
+shopPage();
