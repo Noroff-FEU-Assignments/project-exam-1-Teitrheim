ce9903bc4b7b3344889b243159c3d853fb1d8309
Thomas Eitrheim <teitrheim@gmail.com>
2023-09-22 13:36:51 +0200

    made hamburger menu

--- JS/index.js
+++ JS/index.js
@@ -63,3 +63,10 @@ async function shopPage() {
 }
 
 shopPage();
+
+const mobileNav = document.getElementById("mobileNav");
+const mobileMenuButton = document.querySelector(".mobileMenu");
+
+mobileMenuButton.addEventListener("click", () => {
+  mobileNav.classList.toggle("active");
+});
