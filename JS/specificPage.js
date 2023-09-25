16b0577f899d9dc88a10c51241aa7a8ad0226b7d
Thomas Eitrheim <teitrheim@gmail.com>
2023-09-25 16:15:10 +0200

    minor adjustments

--- JS/specificPage.js
+++ JS/specificPage.js
@@ -46,7 +46,6 @@ document.addEventListener("DOMContentLoaded", async () => {
 
       container.innerHTML += postHtml;
 
-      // Update the page title based on the blog's title
       document.getElementById(
         "blogTitle"
       ).textContent = `Fighting Days | ${post.title.rendered}`;
