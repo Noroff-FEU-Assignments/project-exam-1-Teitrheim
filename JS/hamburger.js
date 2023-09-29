5fe0325c13f0c9569ead7e39485ee85b7b1113bb
Thomas Eitrheim <teitrheim@gmail.com>
2023-09-29 21:12:25 +0200

    improving user experience

--- JS/hamburger.js
+++ JS/hamburger.js
@@ -4,3 +4,9 @@ const mobileMenuButton = document.querySelector(".mobileMenu");
 mobileMenuButton.addEventListener("click", () => {
   mobileNav.classList.toggle("active");
 });
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 800) {
+    mobileNav.classList.remove("active");
+  }
+});
